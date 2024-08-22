6e30feafdb910ec576a8ab3ba54fdd15485b4bf3
damirbik <dbikchurayev@bk.ru>
2024-08-22 22:44:49 +0300

    SF-24

--- SimpleFuzzy/SimpleFuzzy.Abstract/ICompileService.cs
+++ SimpleFuzzy/SimpleFuzzy.Abstract/ICompileService.cs
@@ -1,7 +1,18 @@
-﻿namespace SimpleFuzzy.Abstract
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Text;
+
+namespace SimpleFuzzy.Abstract
 {
     public interface ICompileService
     {
-        public void Compile(string exeCode, string savePath);
+        public static SyntaxTree Parse(string text, string filename = "", CSharpParseOptions options = null)
+        {
+            var stringText = SourceText.From(text, Encoding.UTF8);
+            return SyntaxFactory.ParseSyntaxTree(stringText, options, filename);
+        }
+
+        public static void Compile(string exeCode, string savePath) { }
     }
 }
--- SimpleFuzzy/SimpleFuzzy.Service/CompileService.cs
+++ SimpleFuzzy/SimpleFuzzy.Service/CompileService.cs
@@ -1,29 +1,60 @@
-﻿using SimpleFuzzy.Abstract;
-using System.CodeDom.Compiler;
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Text;
+using SimpleFuzzy.Abstract;
 
 namespace SimpleFuzzy.Service
 {
     public class CompileService : ICompileService
     {
-        public void Compile(string exeCode, string savePath)
-        {
-            var compiler = CodeDomProvider.CreateProvider("CSharp");
-
-            CompilerParameters cp = new CompilerParameters();
-
-            cp.GenerateExecutable = false;
-
-            cp.OutputAssembly = "CompiledOutput.dll";
-
-            if (Directory.Exists(savePath))
+        private static readonly IEnumerable<string> DefaultNamespaces =
+            new[]
             {
-                if (compiler.Supports(GeneratorSupport.Resources))
-                {
-                    cp.EmbeddedResources.Add(savePath);
-                }
-            }
+                "System",
+                "System.IO",
+                "System.Net",
+                "System.Linq",
+                "System.Text",
+                "System.Text.RegularExpressions",
+                "System.Collections.Generic"
+            };
 
-            CompilerResults cr = compiler.CompileAssemblyFromFile(cp, exeCode);
+        private static string runtimePath = @"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5.1\{0}.dll";
+
+        private static readonly IEnumerable<MetadataReference> DefaultReferences =
+            new[]
+            {
+                MetadataReference.CreateFromFile(string.Format(runtimePath, "mscorlib")),
+                MetadataReference.CreateFromFile(string.Format(runtimePath, "System")),
+                MetadataReference.CreateFromFile(string.Format(runtimePath, "System.Core"))
+            };
+
+        private static readonly CSharpCompilationOptions DefaultCompilationOptions =
+            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
+                    .WithOverflowChecks(true).WithOptimizationLevel(OptimizationLevel.Release)
+                    .WithUsings(DefaultNamespaces);
+
+        public static SyntaxTree Parse(string text, string filename = "", CSharpParseOptions options = null)
+        {
+            var stringText = SourceText.From(text, Encoding.UTF8);
+            return SyntaxFactory.ParseSyntaxTree(stringText, options, filename);
+        }
+
+        public static void Compile(string exeCode, string savePath)
+        {
+            var source = exeCode;
+            var parsedSyntaxTree = Parse(source, "", CSharpParseOptions.Default.WithLanguageVersion(LanguageVersion.CSharp6));
+
+            var compilation = CSharpCompilation.Create("Test.dll", new SyntaxTree[] { parsedSyntaxTree }, DefaultReferences, DefaultCompilationOptions);
+            string[] strings = savePath.Split('\\');
+            savePath = "";
+            for (int i = 0; i < strings.Length - 1; i++)
+            {
+                if (strings[i] != "") savePath = savePath + strings[i] + "\\";
+            }
+            var result = compilation.Emit(savePath);
         }
     }
 }
+
